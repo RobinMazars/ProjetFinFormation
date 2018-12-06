97d72d16ebfeab3a7229affb2f4a1741a07596f1
RobinMazars <45461136+RobinMazars@users.noreply.github.com>
2018-12-06 17:57:01 +0100

    scroll menu with decoration

--- js/main.js
+++ js/main.js
@@ -1,12 +1,29 @@
+//init
+$(".sousMenusListe").hide()
+$(".sousMenusListe").find(':first-child').css('border-radius', '10px 10px 0 0');
+$(".sousMenusListe").find(':last-child').css('border-radius', '0 0 10px 10px');
+$(".sousMenusListe").width(function () {
+  var parent=$(this).parent();
+  var paddingParent=parseInt(parent.css('padding-left'));
+  var positionParentLeft=parent.offset().left;
+  console.log (positionParentLeft+2*paddingParent);
+  $(this).css("left",239)
+  $(this).width($(".sousMenusListe").parent().width()-paddingParent)
+})
 //bind event/////////////////////
-$("nav ul li a").hover(function() {
+$("nav ul li").hover(function() {
   if ($(window).width() > 780) {
     $(this).css('background', 'red');
-    $(this).css('color', 'white');
+    $(this).children().css('color', 'white');
   }
 }, function() {
   $(this).css('background', '');
-  $(this).css('color', '');
+  $(this).children().css('color', '');
+});
+$("nav ul li.sousMenusTitle").hover(function() {
+$(this).children(".sousMenusListe").show()
+}, function() {
+  $(this).children(".sousMenusListe").hide()
 });
 // TODO: implement
 //resize event
